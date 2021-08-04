b704bc39d85c1c94d825cf9027b0e640b4bcff5b
briggySmalls <briggySmalls90@gmail.com>
2021-08-04 18:12:50 +0100

    Update intents and code

--- lambda/index.js
+++ lambda/index.js
@@ -1,16 +1,22 @@
 /* *
- * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
- * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * This sample demonstrates handling intents for an Alexa skill implementing the AudioPlayer interface using the Alexa Skills Kit SDK (v2).
+ * This sample works using the default DynamoDB table associated with an Alexa-hosted skill - you will need to use this with a hosted skill,
+ * or you use your own DynamoDB table in the request and response interceptors.
+ * Please visit https://github.com/alexa-samples for additional examples on implementing slots, dialog management,
  * session persistence, api calls, and more.
  * */
 const Alexa = require('ask-sdk-core');
+const AWS = require('aws-sdk');
+const ddbAdapter = require('ask-sdk-dynamodb-persistence-adapter');
+const Util = require('./util.js');
+const podcastUrl = "https://quayfm.apps.sambriggs.dev/live";
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Welcome, you can say Hello or Help. Which would you like to try?';
+        const speakOutput = 'Welcome, you can say "play audio" to start listening to music. What would you like to do?';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -18,18 +24,83 @@ const LaunchRequestHandler = {
             .getResponse();
     }
 };
-
-const HelloWorldIntentHandler = {
+/**
+ * Intent handler to start playing an audio file.
+ * By default, it will play a specific audio stream.
+ * */
+const PlayAudioIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'PlayAudioIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.ResumeIntent');
     },
-    handle(handlerInput) {
-        const speakOutput = 'Hello World!';
+    async handle(handlerInput) {
+        const playbackInfo = await getPlaybackInfo(handlerInput);
+
+        const speakOutput = 'Playing the audio stream.';
+        const playBehavior = 'REPLACE_ALL';
+
+        /**
+         * If your audio file is located on the S3 bucket in a hosted skill, you can use the line below to retrieve a presigned URL for the audio file.
+         * https://developer.amazon.com/docs/alexa/hosted-skills/alexa-hosted-skills-media-files.html
+         *
+         * const podcastUrl = Util.getS3PreSignedUrl("Media/audio.mp3").replace(/&/g,'&amp;');
+         *
+         * If you cannot play your own audio in place of the sample URL, make sure your audio file adheres to the guidelines:
+         * https://developer.amazon.com/docs/alexa/custom-skills/audioplayer-interface-reference.html#audio-stream-requirements
+        */
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .addAudioPlayerPlayDirective(
+                playBehavior,
+                podcastUrl,
+                playbackInfo.token,
+                playbackInfo.offsetInMilliseconds
+            )
+            .getResponse();
+    }
+};
+
+/**
+ * Intent handler to start playing an audio file.
+ * By default, it will play a specific audio stream.
+ * */
+const PauseAudioIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.PauseIntent';
+    },
+    async handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .addAudioPlayerStopDirective()
+            .getResponse();
+    }
+};
+/**
+ * Intent handler for built-in intents that aren't supported in this sample skill.
+ * As this is a sample skill for a single stream, these intents are irrelevant to this skill.
+ * Regardless, the skill needs to handle this gracefully, which is why this handler exists.
+ * */
+const UnsupportedAudioIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (
+                Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.LoopOffIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.LoopOnIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.NextIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.PreviousIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.RepeatIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.ShuffleOffIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.ShuffleOnIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StartOverIntent'
+            );
+    },
+    async handle(handlerInput) {
+        const speakOutput = 'Sorry, I can\'t support that yet.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
             .getResponse();
     }
 };
@@ -40,7 +111,7 @@ const HelpIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'You can say hello to me! How can I help?';
+        const speakOutput = 'You can say "play audio" to start playing music! How can I help?';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -64,9 +135,111 @@ const CancelAndStopIntentHandler = {
     }
 };
 /* *
+ * AudioPlayer events can be triggered when users interact with your audio playback, such as stopping and
+ * starting the audio, as well as when playback is about to finish playing or playback fails.
+ * This handler will save the appropriate details for each event and log the details of the exception,
+ * which can help troubleshoot issues with audio playback.
+ * */
+const AudioPlayerEventHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type.startsWith('AudioPlayer.');
+    },
+    async handle(handlerInput) {
+        const playbackInfo = await getPlaybackInfo(handlerInput);
+
+        const audioPlayerEventName = handlerInput.requestEnvelope.request.type.split('.')[1];
+        console.log(`AudioPlayer event encountered: ${handlerInput.requestEnvelope.request.type}`);
+        let returnResponseFlag = false;
+        switch (audioPlayerEventName) {
+            case 'PlaybackStarted':
+                playbackInfo.token = handlerInput.requestEnvelope.request.token;
+                playbackInfo.inPlaybackSession = true;
+                playbackInfo.hasPreviousPlaybackSession = true;
+                returnResponseFlag = true;
+                break;
+            case 'PlaybackFinished':
+                playbackInfo.inPlaybackSession = false;
+                playbackInfo.hasPreviousPlaybackSession = false;
+                playbackInfo.nextStreamEnqueued = false;
+                returnResponseFlag = true;
+                break;
+            case 'PlaybackStopped':
+                playbackInfo.token = handlerInput.requestEnvelope.request.token;
+                playbackInfo.inPlaybackSession = true;
+                playbackInfo.offsetInMilliseconds = handlerInput.requestEnvelope.request.offsetInMilliseconds;
+                break;
+            case 'PlaybackNearlyFinished':
+                break;
+            case 'PlaybackFailed':
+                playbackInfo.inPlaybackSession = false;
+                console.log('Playback Failed : %j', handlerInput.requestEnvelope.request.error);
+                break;
+            default:
+                break;
+        }
+        setPlaybackInfo(handlerInput, playbackInfo);
+        return handlerInput.responseBuilder.getResponse();
+    },
+};
+
+
+/* *
+ * PlaybackController events can be triggered when users interact with the audio controls on a device screen.
+ * starting the audio, as well as when playback is about to finish playing or playback fails.
+ * This handler will save the appropriate details for each event and log the details of the exception,
+ * which can help troubleshoot issues with audio playback.
+ * */
+const PlaybackControllerHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type.startsWith('PlaybackController.');
+    },
+    async handle(handlerInput) {
+        const playbackInfo = await getPlaybackInfo(handlerInput);
+        const playBehavior = 'REPLACE_ALL';
+        const playbackControllerEventName = handlerInput.requestEnvelope.request.type.split('.')[1];
+        let response;
+        switch (playbackControllerEventName) {
+            case 'PlayCommandIssued':
+                response = handlerInput.responseBuilder
+                    .addAudioPlayerPlayDirective(
+                        playBehavior,
+                        podcastUrl,
+                        playbackInfo.token,
+                        playbackInfo.offsetInMilliseconds
+                    )
+                    .getResponse();
+                break;
+            case 'PauseCommandIssued':
+                response = handlerInput.responseBuilder
+                    .addAudioPlayerStopDirective()
+                    .getResponse();
+                break;
+            default:
+                break;
+        }
+        setPlaybackInfo(handlerInput, playbackInfo);
+
+        console.log(`PlayCommandIssued event encountered: ${handlerInput.requestEnvelope.request.type}`);
+        return response;
+    },
+};
+/* *
+ * SystemExceptions can be triggered if there is a problem with the audio that is trying to be played.
+ * This handler will log the details of the exception and can help troubleshoot issues with audio playback.
+ * */
+const SystemExceptionHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'System.ExceptionEncountered';
+    },
+    handle(handlerInput) {
+        console.log(`System exception encountered: ${JSON.stringify(handlerInput.requestEnvelope.request)}`);
+    },
+};
+
+/* *
  * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
  * It must also be defined in the language model (if the locale supports it)
- * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * This handler can be safely added but will be ingnored in locales that do not support it yet
  * */
 const FallbackIntentHandler = {
     canHandle(handlerInput) {
@@ -83,9 +256,9 @@ const FallbackIntentHandler = {
     }
 };
 /* *
- * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
- * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
- * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs
  * */
 const SessionEndedRequestHandler = {
     canHandle(handlerInput) {
@@ -99,8 +272,8 @@ const SessionEndedRequestHandler = {
 };
 /* *
  * The intent reflector is used for interaction model testing and debugging.
- * It will simply repeat the intent the user said. You can create custom handlers for your intents 
- * by defining them above, then also adding them to the request handler chain below 
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents
+ * by defining them above, then also adding them to the request handler chain below
  * */
 const IntentReflectorHandler = {
     canHandle(handlerInput) {
@@ -119,7 +292,7 @@ const IntentReflectorHandler = {
 /**
  * Generic error handling to capture any syntax or routing errors. If you receive an error
  * stating the request handler chain is not found, you have not implemented a handler for
- * the intent being invoked or included it in the skill builder below 
+ * the intent being invoked or included it in the skill builder below
  * */
 const ErrorHandler = {
     canHandle() {
@@ -136,21 +309,83 @@ const ErrorHandler = {
     }
 };
 
+/* HELPER FUNCTIONS */
+
+async function getPlaybackInfo(handlerInput) {
+    const attributes = await handlerInput.attributesManager.getPersistentAttributes();
+    return attributes.playbackInfo;
+}
+
+async function setPlaybackInfo(handlerInput, playbackInfoObject) {
+    await handlerInput.attributesManager.setPersistentAttributes({
+        playbackInfo: playbackInfoObject
+    });
+}
+
+// Request and response interceptors using the DynamoDB table associated with Alexa-hosted skills
+
+const LoadPersistentAttributesRequestInterceptor = {
+    async process(handlerInput) {
+        const persistentAttributes = await handlerInput.attributesManager.getPersistentAttributes();
+
+        /**
+         * Check if user is invoking the skill the first time and initialize preset values
+            playbackInfo: {
+                  offsetInMilliseconds - this is used to set the offset of the audio file
+                            to save the position between sessions
+                  token - save an audio token for this play session
+                  inPlaybackSession - used to record the playback state of the session
+                  hasPreviousPlaybackSession - used to help confirm previous playback state
+                }
+        */
+        if (Object.keys(persistentAttributes).length === 0) {
+            handlerInput.attributesManager.setPersistentAttributes({
+                playbackInfo: {
+                    offsetInMilliseconds: 0,
+                    token: 'sample-audio-token',
+                    inPlaybackSession: false,
+                    hasPreviousPlaybackSession: false,
+                },
+            });
+        }
+    },
+};
+
+const SavePersistentAttributesResponseInterceptor = {
+    async process(handlerInput) {
+        await handlerInput.attributesManager.savePersistentAttributes();
+    },
+};
+
 /**
  * This handler acts as the entry point for your skill, routing all request and response
  * payloads to the handlers above. Make sure any new handlers or interceptors you've
- * defined are included below. The order matters - they're processed top to bottom 
+ * defined are included below. The order matters - they're processed top to bottom
  * */
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
-        HelloWorldIntentHandler,
+        PlayAudioIntentHandler,
+        PauseAudioIntentHandler,
+        UnsupportedAudioIntentHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
+        AudioPlayerEventHandler,
+        PlaybackControllerHandler,
+        SystemExceptionHandler,
         FallbackIntentHandler,
         SessionEndedRequestHandler,
         IntentReflectorHandler)
     .addErrorHandlers(
         ErrorHandler)
-    .withCustomUserAgent('sample/hello-world/v1.2')
+    .addRequestInterceptors(LoadPersistentAttributesRequestInterceptor)
+    .addResponseInterceptors(SavePersistentAttributesResponseInterceptor)
+    .withCustomUserAgent('sample/audioplayer-nodejs/v2.0')
+    .withPersistenceAdapter(
+        new ddbAdapter.DynamoDbPersistenceAdapter({
+            tableName: process.env.DYNAMODB_PERSISTENCE_TABLE_NAME,
+            createTable: false,
+            dynamoDBClient: new AWS.DynamoDB({ apiVersion: 'latest', region: process.env.DYNAMODB_PERSISTENCE_REGION })
+        })
+    )
     .lambda();
